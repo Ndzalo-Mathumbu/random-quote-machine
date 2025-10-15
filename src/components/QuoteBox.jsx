7a311349b5c7f26aed943c1576c02c41c68b624a
Ndzalo-Mathumbu <ndzalonkmathumbu@gmail.com>
2025-10-15 23:36:15 +0200

    add a random color generator

--- src/components/QuoteBox.jsx
+++ src/components/QuoteBox.jsx
@@ -1,6 +1,26 @@
 import React from "react";
 import "./QuoteBox.scss";
+import $ from "jquery";
+
 const QUOTEBOX = function () {
+  const generateRandomColor = () => {
+    const color1 = Math.floor(Math.random() * 255);
+    const color2 = Math.floor(Math.random() * 255);
+    const color3 = Math.floor(Math.random() * 255);
+    const rgbNum1 = `rgb(${color1}, ${color2}, ${color3})`;
+    const pickLowestNumber = Math.min(color1, color2, color3);
+    const pickHighestNumber = Math.max(color1, color2, color3);
+    const genRandomNumBetweenPickLowestPickHighest =
+      Math.floor(Math.random() * (pickHighestNumber - pickLowestNumber + 1)) +
+      pickLowestNumber;
+
+    const rgbNum2 = `rgb(${pickLowestNumber}, ${pickHighestNumber}, ${genRandomNumBetweenPickLowestPickHighest})`;
+    // const b = Math.max(color1, 0, 255);
+    $("html").css("--blue-primary", rgbNum1);
+    $("html").css("--back-primary", rgbNum2);
+    console.log(rgbNum1, rgbNum2);
+  };
+
   return (
     <div className="quote-box container">
       <p>
@@ -12,7 +32,7 @@ const QUOTEBOX = function () {
         Quis fuga id error. Quo, pariatur inventore.
       </p>
       <h4>~ Ndzalo NK Mathumbu</h4>
-      <button>New Quote</button>
+      <button onClick={generateRandomColor}>New Quote</button>
       <div className="socials container">
         <a href="#">
           <img src="./twitter.png" className="img-fluid" alt="twitter" />
